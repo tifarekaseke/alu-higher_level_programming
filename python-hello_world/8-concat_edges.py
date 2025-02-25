01fa888f444782a659d221230fb0f7d60baa9844
root <tifarekaseke>
2025-02-25 04:49:24 -0800

    Prints a specific concatenated string.

--- python-hello_world/8-concat_edges.py
+++ python-hello_world/8-concat_edges.py
@@ -1,5 +1,6 @@
 #!/usr/bin/python3
 str = "Python is an interpreted, interactive, object-oriented programming\
-        language that combines remarkable power with very clear syntax"
-str = str[39:66] + str[106:112] + str[:6]  # Extract required parts
+         language that combines remarkable power with very clear syntax"
+str = str[39:66] + str[107:113] + str[:6]  # Correct slices
 print(str)
+
