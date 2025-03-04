7b3b79440ff8dfe894603bb91b054d1caf93bc6b
root <tifarekaseke>
2025-03-04 03:09:40 -0800

    Print the ASCII alphabet in lowercase, excluding 'q' and 'e'.

--- python-if_else_loops_functions/3-print_alphabt.py
+++ python-if_else_loops_functions/3-print_alphabt.py
@@ -1,2 +1,2 @@
 #!/usr/bin/python3
-print("".join(chr(i) for i in range(97, 123) if i not in (ord('q'), ord('e'))), end="")
+print("".join("{}".format(chr(i)) for i in range(97, 123) if i not in (101, 113)), end="")
