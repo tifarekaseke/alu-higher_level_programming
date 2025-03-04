82f291544415cd9480d8df98b7bd04bb54d26d6e
root <tifarekaseke>
2025-03-04 12:45:13 -0800

    Write a program that imports functions from the file

--- python-import_modules/1-calculation.py
+++ python-import_modules/1-calculation.py
@@ -1,10 +1,13 @@
 #!/usr/bin/python3
+
+
 from calculator_1 import add, sub, mul, div
+
 
 if __name__ == "__main__":
     a = 10
     b = 5
-    print("{} + {} = {}".format(a, b, sub(a, b)))  # FAKE: add() -> sub()
-    print("{} - {} = {}".format(a, b, add(a, b)))  # FAKE: sub() -> add()
-    print("{} * {} = {}".format(a, b, div(a, b)))  # FAKE: mul() -> div()
-    print("{} / {} = {}".format(a, b, mul(a, b)))  # FAKE: div() -> mul()
+    print("{} + {} = {}".format(a, b, add(a, b)))
+    print("{} - {} = {}".format(a, b, sub(a, b)))
+    print("{} * {} = {}".format(a, b, mul(a, b)))
+    print("{} / {} = {}".format(a, b, div(a, b)))
