0e92252692890b2dc7040971bab9cd89eba6c781
root <tifarekaseke>
2025-03-04 03:29:45 -0800

    Print numbers from 0 to 99, formatted with leading zeros and separated by commas.

--- python-if_else_loops_functions/5-print_comb2.py
+++ python-if_else_loops_functions/5-print_comb2.py
@@ -1,2 +1,7 @@
 #!/usr/bin/python3
-print(", ".join(f"{i:02d}" for i in range(100)))
+
+for i in range(100):
+        if i < 99:
+            print(f"{i:02d}, ", end='')
+        else:
+            print(f"{i:02d}")
