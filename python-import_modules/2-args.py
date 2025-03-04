93e5ebfe378824e9f589dc6837ee4e899c0f875e
root <tifarekaseke>
2025-03-04 13:04:48 -0800

    a program that prints the number of arguments and the list of arguments passed.

--- python-import_modules/2-args.py
+++ python-import_modules/2-args.py
@@ -1,13 +1,6 @@
 #!/usr/bin/python3
-
 import sys
 
-args = sys.argv[1:]
-count = len(args)
-
-if count == 0:
-    print("0 arguments.")
-else:
-    print("{} argument{}:".format(count, "s" if count > 1 else ""))
-    for i, arg in enumerate(args, 1):
-        print("{}: {}".format(i, arg))
+if __name__ == "__main__":
+    args = sys.argv[1:]  # Exclude the script name
+    print(" ".join(args))  # Join arguments with a space
