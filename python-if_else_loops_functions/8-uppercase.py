14998ae70fb7cc5ecae7978006a24af9245dbe91
root <tifarekaseke>
2025-03-04 04:22:15 -0800

    Write a function that prints a string in uppercase

--- python-if_else_loops_functions/8-uppercase.py
+++ python-if_else_loops_functions/8-uppercase.py
@@ -1,8 +1,10 @@
 #!/usr/bin/python3
 
-def uppercase(str):
-    for char in str:
-        if 'a' <= char <= 'z':
-            char = chr(ord(char) - 32)
-        print(char, end="")
-    print()
+def uppercase(string):
+    new_str = ""
+    for char in string:
+        if ord(char) >= 97 and ord(char) <= 122:
+            new_str += chr(ord(char) - 32)
+        else:
+            new_str += char
+    print("{}".format(new_str))
