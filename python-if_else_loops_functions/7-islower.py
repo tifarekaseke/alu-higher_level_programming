b8928b4fe75fd6f8bdb34cf49ada7887a9312394
root <tifarekaseke>
2025-03-04 04:00:37 -0800

    Write a function to check if a character is lowercase.

--- python-if_else_loops_functions/7-islower.py
+++ python-if_else_loops_functions/7-islower.py
@@ -1,4 +1,4 @@
-#!/usr/bin/python3
-
-def islower(c):
-        return ord('a') <= ord(c) <= ord('z')
+slower(c):
+        if ord(c) >= 97 and ord(c) <= 122:
+            return True
+        return False
