2cd1b18d503886e128433af9a10ea4d6ac53ccf1
tifarekaseke <t.kaseke@alustudent.com>
2025-04-04 15:39:57 +0530

    Update 7-add_item.py

--- python-input_output/7-add_item.py
+++ python-input_output/7-add_item.py
@@ -1,15 +1,24 @@
 #!/usr/bin/python3
-"""Add all arguments to a Python list and save them to a file."""
 import sys
+from 5-save_to_json_file import save_to_json_file
+from 6-load_from_json_file import load_from_json_file
 
-if __name__ == "__main__":
-    save_to_json_file = __import__('5-save_to_json_file').save_to_json_file
-    load_from_json_file = \
-        __import__('6-load_from_json_file').load_from_json_file
+def main():
+    # Initialize the file path
+    file_path = "add_item.json"
 
+    # Try to load the existing list from the JSON file
     try:
-        items = load_from_json_file("add_item.json")
+        items = load_from_json_file(file_path)
     except FileNotFoundError:
         items = []
+
+    # Add the arguments (excluding the script name)
     items.extend(sys.argv[1:])
-    save_to_json_file(items, "add_item.json")
+
+    # Save the updated list to the JSON file
+    save_to_json_file(items, file_path)
+
+if __name__ == "__main__":
+    main()
+
